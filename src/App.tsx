17e68ff6e45a098920318f1491371ebab94dc385
bender <kudin.dima@gmail.com>
2026-07-19 13:41:05 +0000

    Switch to version 27: modified src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 
-import HomeRussianPage from "@/pages/HomeRussianPage";
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/home-russian" element={<HomeRussianPage />} />
       </Route>
     </Routes>
   );
